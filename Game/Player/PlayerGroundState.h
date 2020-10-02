818fa05cd8e6f98ed717bd6e6c89db9956f7da28
ZekeZR1 <hnta3574@gmail.com>
2020-10-03 00:48:42 +0900

    ちょっとノロめに

--- Game/Player/PlayerGroundState.h
+++ Game/Player/PlayerGroundState.h
@@ -16,7 +16,7 @@ public:
 
 private:
 	const float m_VELOCITY_MAX = 20.f;
-	const float m_QUICKNESS = 40.f;
+	const float m_QUICKNESS = 30.f;
 	Vector3 m_velocity = Vector3::Zero;
 	Vector3 m_vecVelocityGoal = Vector3::Zero;
 };
