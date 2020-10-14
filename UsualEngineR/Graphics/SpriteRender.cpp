1e948cc434b4b946a2f876eaab3e0d860c499aa6
kokoasann <kokoasann@gmail.com>
2020-10-14 10:18:31 +0900

    sprite render

---
+++ UsualEngineR/Graphics/SpriteRender.cpp
@@ -0,0 +1,23 @@
+#include "PreCompile.h"
+#include "SpriteRender.h"
+
+
+namespace UER
+{
+	void SpriteRender::OnDestroy()
+	{
+	}
+	void SpriteRender::Init(const SpriteInitData& sid)
+	{
+		mSprite.Init(sid);
+	}
+	void SpriteRender::Update()
+	{
+		mSprite.Update(mPosition, mRotation, mScale, mPivot);
+	}
+	void SpriteRender::PostRender()
+	{
+		auto& rc = g_graphicsEngine->GetRenderContext();
+		mSprite.Draw(rc);
+	}
+}
---
+++ UsualEngineR/Graphics/SpriteRender.h
@@ -0,0 +1,75 @@
+#pragma once
+
+
+namespace UER
+{
+	class SpriteRender :public GameObject
+	{
+	public:
+		SpriteRender() {};
+		~SpriteRender() {};
+
+		void OnDestroy();
+
+		void Init(const SpriteInitData& sid);
+
+		void Update();
+
+		void PostRender() override;
+
+		/*
+			mPositionにセットする
+		*/
+		void SetPos(Vector3 pos)
+		{
+			mPosition = pos;
+		}
+		
+		/*
+			mRotationにセットする
+		*/
+		void SetRot(Quaternion rot)
+		{
+			mRotation = rot;
+		}
+
+		/*
+			mScaleにセットする
+		*/
+		void SetSca(Vector3 sca)
+		{
+			mScale = sca;
+		}
+
+		/*
+			mPositionをゲットする
+		*/
+		Vector3 GetPos()
+		{
+			return mPosition;
+		}
+
+		/*
+			mRotationをゲットする
+		*/
+		Quaternion GetRot()
+		{
+			return mRotation;
+		}
+
+		/*
+			mScaleをゲットする
+		*/
+		Vector3 GetSca()
+		{
+			return mScale;
+		}
+	private:
+		Sprite mSprite;											//スプライト
+		Vector3 mPosition = g_vec3Zero;					//ポジション
+		Quaternion mRotation = g_quatIdentity;		//ローテーション
+		Vector3 mScale = g_vec3One;						//スケール
+		Vector2 mPivot = {0.5f,0.5f};							//ピボット
+
+	};
+}
