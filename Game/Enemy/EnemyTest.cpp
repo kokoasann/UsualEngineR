7510ff88b11b14a7ab34c187da0fe5584d43b607
Zeke <hnta3574@gmail.com>
2020-10-16 12:04:37 +0900

    敵のモデルを変更

--- Game/Enemy/EnemyTest.cpp
+++ Game/Enemy/EnemyTest.cpp
@@ -6,24 +6,24 @@
 void EnemyTest::Init() {
 
 	ModelInitData mid;
-	mid.m_vsfxFilePath = "Assets/shader/AnimModel.fx";
+	mid.m_tkmFilePath = "Assets/modelData/test/test_criss.tkm";
 	mid.m_vsEntryPointFunc = "VSMain";
 	mid.m_psEntryPointFunc = "PSMain";
-	mid.m_tkmFilePath = "Assets/modelData/unityChan.tkm";
-	mid.m_tksFilePath = "Assets/modelData/unityChan.tks";
+	//mid.m_tkmFilePath = "Assets/modelData/unityChan.tkm";
+	//mid.m_tksFilePath = "Assets/modelData/unityChan.tks";
 	mid.m_upAxis = enUpAxisY;
 
 	m_model = NewGO<ModelRender>(0);
 	m_model->Init(mid);
-	m_model->SetScale(g_vec3One * 0.05);
+	m_model->SetScale(g_vec3One);
 	m_animlist.resize(1);
 	m_animlist[0] = std::make_unique<CAnimationClip>();
 	m_animlist[0]->Load("Assets/anim/unityChan/run.tka");
 	m_animlist[0]->BuildKeyFramesAndAnimationEvents();
 	m_animlist[0]->SetLoopFlag(true);
 
-	m_model->InitAnimation(m_animlist, 1);
-	m_model->Play(0);
+	//m_model->InitAnimation(m_animlist, 1);
+	//m_model->Play(0);
 
 	SetState(m_stateList[static_cast<int>(IEnemy::EnState::enIdleState)]);
 
