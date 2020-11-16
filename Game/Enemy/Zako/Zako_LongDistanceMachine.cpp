123cb3ef94742555963c1e41c28e961a486ab03a
kokoasann <kokoasann@gmail.com>
2020-11-16 14:13:54 +0900

    add zako enemy class

---
+++ Game/Enemy/Zako/Zako_LongDistanceMachine.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "Zako_LongDistanceMachine.h"
+
+
+Zako_LongDistanceMachine::Zako_LongDistanceMachine()
+{
+
+}
+
+Zako_LongDistanceMachine::~Zako_LongDistanceMachine()
+{
+
+}
+
+
+
+void Zako_LongDistanceMachine::Release()
+{
+
+}
+
+void Zako_LongDistanceMachine::OnDestroy()
+{
+	Release();
+}
+
+
+void Zako_LongDistanceMachine::Awake()
+{
+
+}
+
+bool Zako_LongDistanceMachine::Start()
+{
+	return true;
+}
+
+
+void Zako_LongDistanceMachine::PreUpdate()
+{
+
+}
+
+void Zako_LongDistanceMachine::Update()
+{
+
+}
+
+
+
+
---
+++ Game/Enemy/Zako/Zako_ShortDistanceMonster.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "Zako_ShortDistanceMonster.h"
+
+
+Zako_ShortDistanceMonster::Zako_ShortDistanceMonster()
+{
+
+}
+
+Zako_ShortDistanceMonster::~Zako_ShortDistanceMonster()
+{
+
+}
+
+
+
+void Zako_ShortDistanceMonster::Release()
+{
+
+}
+
+void Zako_ShortDistanceMonster::OnDestroy()
+{
+	Release();
+}
+
+
+void Zako_ShortDistanceMonster::Awake()
+{
+
+}
+
+bool Zako_ShortDistanceMonster::Start()
+{
+	return true;
+}
+
+
+void Zako_ShortDistanceMonster::PreUpdate()
+{
+
+}
+
+void Zako_ShortDistanceMonster::Update()
+{
+
+}
+
+
+
+
