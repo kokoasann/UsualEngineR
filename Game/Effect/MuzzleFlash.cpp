7c13a8bc2d59fcf8a1176483840a56d53a428f94
kokoasann <kokoasann@gmail.com>
2020-11-26 10:11:10 +0900

    MuzzleFlash

---
+++ Game/Effect/MuzzleFlash.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "MuzzleFlash.h"
+
+
+MuzzleFlash::MuzzleFlash()
+{
+
+}
+
+MuzzleFlash::~MuzzleFlash()
+{
+
+}
+
+
+
+void MuzzleFlash::Release()
+{
+
+}
+
+void MuzzleFlash::OnDestroy()
+{
+
+}
+
+
+void MuzzleFlash::Awake()
+{
+
+}
+
+bool MuzzleFlash::Start()
+{
+	return true;
+}
+
+
+void MuzzleFlash::PreUpdate()
+{
+
+}
+
+void MuzzleFlash::Update()
+{
+
+}
+
+void MuzzleFlash::PostUpdate()
+{
+
+}
+
+
+
