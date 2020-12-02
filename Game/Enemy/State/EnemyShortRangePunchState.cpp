55f7bb604b14244e7c78b0210a4619c92462645d
kbc18b24 <kbc18b24@stu.kawahara.ac.jp>
2020-12-02 10:47:59 +0900

    攻撃系ステート大まか完成

--- Game/Enemy/State/EnemyShortRangePunchState.cpp
+++ Game/Enemy/State/EnemyShortRangePunchState.cpp
@@ -3,6 +3,7 @@
 #include "Enemy/IEnemy.h"
 #include "Enemy/EnemyManager.h"
 #include "GameManager.h"
+#include "Enemy/Zako/Zako_ShortRangeMonster.h"
 
 EnemyShortRangePunchState::EnemyShortRangePunchState()
 {
@@ -14,24 +15,23 @@ EnemyShortRangePunchState::~EnemyShortRangePunchState()
 
 void EnemyShortRangePunchState::Enter(IEnemy* e)
 {
+
 }
 
 IEnemyState* EnemyShortRangePunchState::Update(IEnemy* e)
 {
-	Attack(e);
-	return this;
+	const float maxSecond = 1.f;
+	m_attackIntervalSecond = m_attackIntervalSecond + gameTime()->GetDeltaTime();
+	//一定時間おきに殴る。
+	if (m_attackIntervalSecond >= maxSecond)
+	{
+		auto& p = GameManager::GetInstance().m_player;
+		p->ApplyDamage(m_damage);
+		m_attackIntervalSecond = 0;
+		return e->GetState(TO_INT(Zako_ShortRangeMonster::EnStateEX::enComing));
+	}
 }
 
 void EnemyShortRangePunchState::Exit(IEnemy* e)
 {
-}
-
-void EnemyShortRangePunchState::Attack(IEnemy* e)
-{
-	//30フレームおきに殴る。
-	if (m_attackIntervalCount >= 30)
-	{
-		auto& p = GameManager::GetInstance().m_player;
-		//p->ApplyDamage();
-	}
 }
--- Game/Enemy/State/EnemyShortRangeStrongPunchState.cpp
+++ Game/Enemy/State/EnemyShortRangeStrongPunchState.cpp
@@ -2,6 +2,8 @@
 #include "EnemyShortRangeStrongPunchState.h"
 #include "Enemy/IEnemy.h"
 #include "Enemy/EnemyManager.h"
+#include "GameManager.h"
+#include "Enemy/Zako/Zako_ShortRangeMonster.h"
 
 EnemyShortRangeStrongPunchState::EnemyShortRangeStrongPunchState()
 {
@@ -13,11 +15,20 @@ EnemyShortRangeStrongPunchState::~EnemyShortRangeStrongPunchState()
 
 void EnemyShortRangeStrongPunchState::Enter(IEnemy* e)
 {
+	
 }
 
 IEnemyState* EnemyShortRangeStrongPunchState::Update(IEnemy* e)
 {
-	return this;
+	const float maxSecond = 1.f;
+	m_attackIntervalSecond = m_attackIntervalSecond + gameTime()->GetDeltaTime();
+	//ˆê’èŽžŠÔ‚¨‚«‚É‰£‚éB
+	if (m_attackIntervalSecond >= maxSecond){
+		auto& p = GameManager::GetInstance().m_player;
+		p->ApplyDamage(m_damage);
+		m_attackIntervalSecond = 0;
+		return e->GetState(TO_INT(Zako_ShortRangeMonster::EnStateEX::enComing));
+	}
 }
 
 void EnemyShortRangeStrongPunchState::Exit(IEnemy* e)
