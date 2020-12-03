18b541c359d811483a6d16ab54e05a3e83999c53
kokoasann <kokoasann@gmail.com>
2020-12-03 10:06:08 +0900

    add explosion

---
+++ Game/Effect/ExplosionEffect.h
@@ -0,0 +1,51 @@
+#pragma once
+
+/// <summary>
+/// 
+/// </summary>
+class ExplosionEffect :public GameObject
+{
+public:
+	ExplosionEffect();
+	virtual ~ExplosionEffect();
+
+	/// <summary>
+	/// 本開放。確保したものを開放するための関数。
+	/// </summary>
+	virtual void Release() override;
+
+	/// <summary>
+	/// このオブジェクトの所持するゲームオブジェクト等を消すための関数。
+	/// 面倒くさければRelease関数と同じでもよい。
+	/// </summary>
+	virtual void OnDestroy() override;
+
+	/// <summary>
+	/// NewGO時に即座に呼ばれる関数。
+	/// コンストラクタの代わりに使おう。
+	/// </summary>
+	void Awake() override;
+
+	/// <summary>
+	/// スタート関数。
+	/// </summary>
+	/// <returns>trueを返さない限り、ずっとStartを呼び続ける</returns>
+	bool Start() override;
+
+	/// <summary>
+	/// 更新。の前に呼ばれる更新。
+	/// </summary>
+	void PreUpdate() override;
+	/// <summary>
+	/// 更新。
+	/// </summary>
+	void Update() override;
+	/// <summary>
+	/// 更新。の後に呼ばれる更新。
+	/// </summary>
+	void PostUpdate() override;
+
+
+private:
+
+};
