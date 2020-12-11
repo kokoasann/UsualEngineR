deebd0ac80f8414882327d1f8b1739d1217c93e8
kokoasann <kokoasann@gmail.com>
2020-12-11 12:03:51 +0900

    add enemy short range long jamp attack state

---
+++ Game/Enemy/State/EnemyShortRangeLongJampAttackState.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "EnemyShortRangeLongJampAttackState.h"
+#include "Enemy/IEnemy.h"
+#include "Enemy/EnemyManager.h"
+
+EnemyShortRangeLongJampAttackState::EnemyShortRangeLongJampAttackState()
+{
+}
+
+EnemyShortRangeLongJampAttackState::~EnemyShortRangeLongJampAttackState()
+{
+}
+
+void EnemyShortRangeLongJampAttackState::Enter(IEnemy* e)
+{
+}
+
+IEnemyState* EnemyShortRangeLongJampAttackState::Update(IEnemy* e)
+{
+	return this;
+}
+
+void EnemyShortRangeLongJampAttackState::Exit(IEnemy* e)
+{
+}
