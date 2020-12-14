b627b107bcb34be47b9c7f46ab244764cce54ed4
kbc18b24 <kbc18b24@stu.kawahara.ac.jp>
2020-12-14 14:00:13 +0900

    Beam追加

---
+++ Game/Effect/Beam.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "Beam.h"
+
+
+Beam::Beam()
+{
+
+}
+
+Beam::~Beam()
+{
+
+}
+
+
+
+void Beam::Release()
+{
+
+}
+
+void Beam::OnDestroy()
+{
+
+}
+
+
+void Beam::Awake()
+{
+
+}
+
+bool Beam::Start()
+{
+	return true;
+}
+
+
+void Beam::PreUpdate()
+{
+
+}
+
+void Beam::Update()
+{
+
+}
+
+void Beam::PostUpdate()
+{
+
+}
+
+
+
