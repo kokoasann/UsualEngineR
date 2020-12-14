e98cc6e330f257eb9fd54988ee2ee892fd8147bb
kbc18b24 <kbc18b24@stu.kawahara.ac.jp>
2020-12-14 13:44:16 +0900

    ステート遷移できるように修正+パラメータ調整+1発だけでなく一度に複数発射できるように仕様変更

--- Game/Enemy/State/BossFatmanState/Boss_FatmanShootingState.cpp
+++ Game/Enemy/State/BossFatmanState/Boss_FatmanShootingState.cpp
@@ -18,67 +18,27 @@ void Boss_FatmanShootingState::Enter(IEnemy* e)
 {
 	const float time = 20.f;
 	m_damage = Boss_Fatman::CalcDamage(time);
+
+	m_shotTimer = 0.f;
+	m_stateTimer = 0.f;
 }
 
 IEnemyState* Boss_FatmanShootingState::Update(IEnemy* e)
 {
-	const float shotTime = 0.03f;
+	const float shotTime = 0.01f;
 	m_shotTimer += gameTime()->GetDeltaTime();
+	m_stateTimer += gameTime()->GetDeltaTime();
+	const float maxTime = 5.f;
+	if (m_stateTimer > maxTime) {
+		return e->GetState(TO_INT(IEnemy::EnState::enBattleState));
+	}
 
 	//一定間隔で弾を撃つ。
 	if (m_shotTimer > shotTime) {
-		//撃ちだす方向を決める。
-		//少し上に設定。
-		const float upY = 6.f;
-		Enemy_Bullet* eb = NewGO<Enemy_Bullet>(0, true);
-		auto epos = e->GetPosition();
-		epos.y += upY;
-
-		auto& p = GameManager::GetInstance().m_player;
-		auto ppos = p->GetPosition();
-		ppos.y += upY;
-		
-		Vector3 dir = ppos - epos;
-		dir.Normalize();
-
-		//撃ちだす弾の振れ幅を決める。(1つ目の回転)
-		Vector3 cross;
-		cross.Cross(dir, Vector3::Up);
-		
-		//ランダムな角度。
-		const float maxAngle = 5.f;
-		float angle = 0.f;
 		auto rand = GRandom().Rand();
-		angle = maxAngle * rand;
-
-		//1つ目の回転を作成。
-		Quaternion rot;
-		rot.SetRotationDeg(cross, angle);
-
-		//主軸を回転させる。(2つ目の回転)
-		//ランダムな角度。
-		const float maxAngle2 = 360.f;
-		float angle2 = 0.f;
-		auto rand2 = GRandom().Rand();
-		angle2 = maxAngle2 * rand2;
-
-		//2つ目の回転を作成。
-		Quaternion rot2;
-		rot2.SetRotationDeg(dir, angle2);
-
-		//回転を適用。
-		rot.Apply(dir);
-		rot2.Apply(dir);
-
-		//弾の初期化。
-		eb->Init(epos, 0.6f, dir, 400.f, 5.f, 2.f);
-		//プレイヤーが飛んでいたら撃ち落とす。
-		const float flyRange = 5.f;
-		if (std::abs(ppos.y - epos.y) > flyRange) {
-			eb->SetDamage(m_damage, true, Vector3::Zero);
-		}
-		else {
-			eb->SetDamage(m_damage);
+		rand *= 3.f;
+		for (int i = 0; i <= rand; i++){
+			BulletGenerate(e);
 		}
 		
 		m_shotTimer = 0.f;
@@ -88,4 +48,65 @@ IEnemyState* Boss_FatmanShootingState::Update(IEnemy* e)
 
 void Boss_FatmanShootingState::Exit(IEnemy* e)
 {
+}
+
+void Boss_FatmanShootingState::BulletGenerate(IEnemy* e)
+{
+	//撃ちだす方向を決める。
+	//少し上に設定。
+	const float upY = 6.f;
+	Enemy_Bullet* eb = NewGO<Enemy_Bullet>(0, true);
+	auto epos = e->GetPosition();
+	epos.y += upY;
+
+	auto& p = GameManager::GetInstance().m_player;
+	auto ppos = p->GetPosition();
+	ppos.y += 4.f;
+
+	Vector3 dir = ppos - epos;
+	dir.Normalize();
+
+	//撃ちだす弾の振れ幅を決める。(1つ目の回転)
+	Vector3 cross;
+	cross.Cross(dir, Vector3::Up);
+
+	//ランダムな角度。
+	const float maxAngle = 4.f;
+	float angle = 0.f;
+	auto rand = GRandom().Rand();
+	angle = maxAngle * rand;
+
+	//1つ目の回転を作成。
+	Quaternion rot;
+	rot.SetRotationDeg(cross, angle);
+
+	//主軸を回転させる。(2つ目の回転)
+	//ランダムな角度。
+	const float maxAngle2 = 360.f;
+	float angle2 = 0.f;
+	auto rand2 = GRandom().Rand();
+	angle2 = maxAngle2 * rand2;
+
+	//2つ目の回転を作成。
+	Quaternion rot2;
+	rot2.SetRotationDeg(dir, angle2);
+
+	//回転を適用。
+	rot.Apply(dir);
+	rot2.Apply(dir);
+
+	//弾の初期化。
+	eb->Init(epos, 0.6f, dir, 400.f, 5.f, 2.f);
+	//プレイヤーが飛んでいたら撃ち落とす。
+	const float flyRange = 5.f;
+	Vector3 knockVector = ppos - epos;
+	knockVector.Normalize();
+	const float knockParam = 200.f;
+	knockVector *= knockParam;
+	if (std::abs(ppos.y - epos.y) > flyRange) {
+		eb->SetDamage(m_damage, true, knockVector);
+	}
+	else {
+		eb->SetDamage(m_damage);
+	}
 }
--- Game/Enemy/State/BossFatmanState/Boss_FatmanShootingState.h
+++ Game/Enemy/State/BossFatmanState/Boss_FatmanShootingState.h
@@ -10,6 +10,12 @@ public:
 	IEnemyState* Update(IEnemy* e) override;
 	void Exit(IEnemy* e) override;
 private:
+	/// <summary>
+	/// ’e‚ğ¶¬‚·‚éŠÖ”B
+	/// </summary>
+	void BulletGenerate(IEnemy* e);
+private:
 	float		m_shotTimer = 0.f;
+	float		m_stateTimer = 0.f;
 	float		m_damage = 0.f;
 };
