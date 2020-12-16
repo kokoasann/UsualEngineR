e2563a791589880f91b82063b42c2afdec498500
kbc18b24 <kbc18b24@stu.kawahara.ac.jp>
2020-12-16 13:55:21 +0900

    基礎完成

--- Game/Effect/Beam.cpp
+++ Game/Effect/Beam.cpp
@@ -53,6 +53,10 @@ void Beam::PostUpdate()
 
 void Beam::Init(const BeamEffectInitData& bid)
 {
+	m_particleScale = bid.particleScale;
+	m_particleColor = bid.particleColor;
+	m_particleLifeTime = bid.particleLifeTime;
+
 	PlaneParticleEffectInitData pid;
 	pid.m_ddsFilePath = L"Assets/Image/illumination.dds";
 	pid.m_height = 10;
@@ -62,12 +66,39 @@ void Beam::Init(const BeamEffectInitData& bid)
 	PlaneParticleUpdater m_effctUpdater(
 		[&]PLANE_PARTICLE_GENERATE_FUNC(pThis, deltaTime)
 	{
-		//pThis->AddParticle(pos, g_vec3One * m_particleScale, g_quatIdentity, m_particleColor, lifeTime, nullptr, false);
+		Vector3 pos = { 0.f,0.f,0.f };
+		Vector3 moveSpeed = Vector3::Zero;
+		const float speed = 5.f;
+		moveSpeed = m_direction * speed;
+		
+		for (int i = 0; i < 100; i++) {
+			pos += moveSpeed;
+			pThis->AddParticle(pos, g_vec3One * m_particleScale, g_quatIdentity, m_particleColor, m_particleLifeTime, nullptr, false);
+		}
+		
+		m_isPlay = false;
+		pThis->SetGenerateFlag(false);
 	},
 		[&]PLANE_PARTICLE_UPDATE_FUNC(data, deltaTime, extendData) {
-
+		
+		//data.particleData.mulColor.Lerp(data.lifeTime/m_particleLifeTime, m_particleColor, m_particleColor);
 	});
+	pid.m_updater = &m_effctUpdater;
+	m_effect = NewGO<PlaneParticleEffectRender>(0);
+	m_effect->Init(pid);
+
+	m_effect->SetGenerateFlag(false);
 }
 
+void Beam::Play()
+{
+	m_isPlay = true;
+	m_effect->SetGenerateFlag(true);
+}
 
+void Beam::Stop()
+{
+	m_isPlay = false;
+	m_effect->SetGenerateFlag(false);
+}
 
