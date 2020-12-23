8eb93efe7c2e053022f0f5ede602cc6e03ac4b69
kokoasann <kokoasann@gmail.com>
2020-12-23 12:07:27 +0900

    debugger

--- Game/GameDebugger.cpp
+++ Game/GameDebugger.cpp
@@ -36,14 +36,20 @@ bool GameDebugger::Start()
 {
 	DebugSwitchAddCheck(DebugSwitchNewSwitch('X', 0, [&]()
 		{
+			if (!GameManager::GetInstance().GetPlayer())
+				return;
 			GameManager::GetInstance().GetPlayer()->Heal(100);
 		}));
 	DebugSwitchAddCheck(DebugSwitchNewSwitch('X', VK_SHIFT, [&]()
 		{
+			if (!GameManager::GetInstance().GetPlayer())
+				return;
 			GameManager::GetInstance().GetPlayer()->ChargeBoost(100);
 		}));
 	DebugSwitchAddCheck(DebugSwitchNewSwitch('X', VK_CONTROL, [&]()
 		{
+			if (!GameManager::GetInstance().GetPlayer())
+				return;
 			GameManager::GetInstance().GetPlayer()->ChargeEndurance(100);
 		}));
 	return true;
