ef43e94e62ce16345becbdb4c726bfb2b82dac5d
kokoasann <kokoasann@gmail.com>
2020-12-23 10:12:20 +0900

    add game debugger

---
+++ Game/GameDebugger.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "GameDebugger.h"
+
+
+GameDebugger::GameDebugger()
+{
+
+}
+
+GameDebugger::~GameDebugger()
+{
+
+}
+
+
+
+void GameDebugger::Release()
+{
+
+}
+
+void GameDebugger::OnDestroy()
+{
+	Release();
+}
+
+
+void GameDebugger::Awake()
+{
+
+}
+
+bool GameDebugger::Start()
+{
+	return true;
+}
+
+
+void GameDebugger::PreUpdate()
+{
+
+}
+
+void GameDebugger::Update()
+{
+
+}
+
+void GameDebugger::PostUpdate()
+{
+
+}
+
+
+
