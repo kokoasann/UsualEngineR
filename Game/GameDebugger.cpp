146a98d721e4637396a43b3e569c1548c32db679
kokoasann <kokoasann@gmail.com>
2021-01-07 02:30:26 +0900

    debugger monster spawner button

--- Game/GameDebugger.cpp
+++ Game/GameDebugger.cpp
@@ -1,7 +1,16 @@
 #include "stdafx.h"
 #include "GameDebugger.h"
 #include "GameManager.h"
+#include "Enemy/EnemyManager.h"
 #include "Player/Player.h"
+
+#include "Enemy/Boss/BossA.h"
+#include "Enemy/Boss/Boss_FatMan.h"
+#include "Enemy/Zako/Zako_ShortRangeMonster.h"
+#include "Enemy/Zako/Zako_LongDistanceMachine.h"
+#include "Enemy/Zako/Zako_ExplosionMonster.h"
+#include "Enemy/Zako/Zako_LongBig.h"
+#include "Enemy/Zako/Zako_ShortBig.h"
 
 
 GameDebugger::GameDebugger()
@@ -51,6 +60,60 @@ bool GameDebugger::Start()
 			if (!GameManager::GetInstance().GetPlayer())
 				return;
 			GameManager::GetInstance().GetPlayer()->ChargeEndurance(100);
+		}));
+
+	DebugSwitchAddButton(DebugSwitchNewSwitch_Button(VK_NUMPAD0, 0, [&]()
+		{
+			if (!GameManager::GetInstance().GetPlayer())
+				return;
+
+			auto e = NewGO<Zako_ShortRangeMonster>(0);
+			IEnemy::StAbility ab;
+			ab.InitStamina(100);
+			ab.InitHP(60);
+			e->SetAbility(ab);
+			e->SetPosition(GameManager::GetInstance().GetPlayer()->GetPosition());
+			EnemyManager::GetEnemyManager().AddEnemy(e);
+			
+		}));
+	DebugSwitchAddButton(DebugSwitchNewSwitch_Button(VK_NUMPAD1, 0, [&]()
+		{
+			if (!GameManager::GetInstance().GetPlayer())
+				return;
+
+			auto e = NewGO<Zako_LongDistanceMachine>(0);
+			IEnemy::StAbility ab;
+			ab.InitStamina(100);
+			ab.InitHP(60);
+			e->SetAbility(ab);
+			e->SetPosition(GameManager::GetInstance().GetPlayer()->GetPosition());
+			EnemyManager::GetEnemyManager().AddEnemy(e);
+		}));
+	DebugSwitchAddButton(DebugSwitchNewSwitch_Button(VK_NUMPAD2, 0, [&]()
+		{
+			if (!GameManager::GetInstance().GetPlayer())
+				return;
+
+			auto e = NewGO<Zako_ShortBig>(0);
+			IEnemy::StAbility ab;
+			ab.InitStamina(100);
+			ab.InitHP(120);
+			e->SetAbility(ab);
+			e->SetPosition(GameManager::GetInstance().GetPlayer()->GetPosition());
+			EnemyManager::GetEnemyManager().AddEnemy(e);
+		}));
+	DebugSwitchAddButton(DebugSwitchNewSwitch_Button(VK_NUMPAD3, 0, [&]()
+		{
+			if (!GameManager::GetInstance().GetPlayer())
+				return;
+
+			auto e = NewGO<Zako_LongBig>(0);
+			IEnemy::StAbility ab;
+			ab.InitStamina(100);
+			ab.InitHP(60);
+			e->SetAbility(ab);
+			e->SetPosition(GameManager::GetInstance().GetPlayer()->GetPosition());
+			EnemyManager::GetEnemyManager().AddEnemy(e);
 		}));
 	return true;
 }
