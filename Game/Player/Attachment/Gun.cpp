5996b293cb8a03b67c198119c576b3ba32119ec8
ZekeZR1 <hnta3574@gmail.com>
2021-01-12 15:52:59 +0900

    アタッチメント追加

---
+++ Game/Player/Attachment/Gun.cpp
@@ -0,0 +1,63 @@
+#include "stdafx.h"
+#include "Gun.h"
+
+
+Gun::Gun()
+{
+
+}
+
+Gun::~Gun()
+{
+
+}
+
+
+
+void Gun::Release()
+{
+
+}
+
+void Gun::OnDestroy()
+{
+	Release();
+}
+
+
+void Gun::Awake()
+{
+
+}
+
+bool Gun::Start()
+{
+	return true;
+}
+
+
+void Gun::PreUpdate()
+{
+
+}
+
+void Gun::Update()
+{
+
+}
+
+void Gun::PostUpdate()
+{
+
+}
+
+
+void Gun::Render()
+{
+
+}
+
+void Gun::PostRender()
+{
+
+}
---
+++ Game/Player/Attachment/Shield.cpp
@@ -0,0 +1,63 @@
+#include "stdafx.h"
+#include "Shield.h"
+
+
+Shield::Shield()
+{
+
+}
+
+Shield::~Shield()
+{
+
+}
+
+
+
+void Shield::Release()
+{
+
+}
+
+void Shield::OnDestroy()
+{
+	Release();
+}
+
+
+void Shield::Awake()
+{
+
+}
+
+bool Shield::Start()
+{
+	return true;
+}
+
+
+void Shield::PreUpdate()
+{
+
+}
+
+void Shield::Update()
+{
+
+}
+
+void Shield::PostUpdate()
+{
+
+}
+
+
+void Shield::Render()
+{
+
+}
+
+void Shield::PostRender()
+{
+
+}
