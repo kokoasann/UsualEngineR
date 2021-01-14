c9e0aa152d7bb14b9182037d26acc64d2a95f876
hirota-ryuki <kbc18b24@stu.kawahara.ac.jp>
2021-01-14 15:50:23 +0900

    攻撃の遷移ほぼ完成

--- Game/Enemy/State/BossFatmanState/Boss_FatmanMainState.cpp
+++ Game/Enemy/State/BossFatmanState/Boss_FatmanMainState.cpp
@@ -27,39 +27,39 @@ IEnemyState* Boss_FatmanMainState::Update(IEnemy* e)
 {
 	m_fatTimer += gameTime()->GetDeltaTime();
 	if (m_fatTimer > 0.3f) {
-		auto& p = GameManager::GetInstance().m_player;
+		/*auto& p = GameManager::GetInstance().m_player;
 		const auto& ppos = p->GetPosition();
-		return e->GetState(TO_INT(IEnemy::EnState::enAttackB));
+		return e->GetState(TO_INT(IEnemy::EnState::enAttackA));*/
 
-		//auto player = GameManager::GetInstance().m_player;
+		auto player = GameManager::GetInstance().m_player;
 
-		//if (player->GetCurrentHP() > 0.f) {
-		//	auto& epos = e->GetPosition();
-		//	auto& ppos = player->GetPosition();
-		//	auto vecToPlayer = ppos - epos;
+		if (player->GetCurrentHP() > 0.f) {
+			auto& epos = e->GetPosition();
+			auto& ppos = player->GetPosition();
+			auto vecToPlayer = ppos - epos;
 
-		//	float diffY = ppos.y - epos.y;
-		//	const float flyRange = 5.f;
-		//	const float attackRange = 75.f;
-		//	//離れたら遠距離攻撃。
-		//	//もしくはプレイヤーが飛んでいたら遠距離攻撃。
-		//	if (vecToPlayer.Length() > attackRange or diffY > flyRange) {
-		//		return LongRangeAttack(e);
-		//	}
-		//	//近づいたらタックルか距離をとる。
-		//	else {
-		//		//確率で行動変化。
-		//		auto rand = GRandom().Rand();
-		//		if (rand < 0.5f) {
-		//			//タックル。
-		//			return e->GetState(TO_INT(Boss_Fatman::EnStateEX::enAttackD));
-		//		}
-		//		else {
-		//			//距離をとる。
-		//			return e->GetState(TO_INT(Boss_Fatman::EnStateEX::enTakeDistance));
-		//		}
-		//	}
-		//}
+			float diffY = ppos.y - epos.y;
+			const float flyRange = 5.f;
+			const float attackRange = 75.f;
+			//離れたら遠距離攻撃。
+			//もしくはプレイヤーが飛んでいたら遠距離攻撃。
+			if (vecToPlayer.Length() > attackRange or diffY > flyRange) {
+				return LongRangeAttack(e);
+			}
+			//近づいたらタックルか距離をとる。
+			else {
+				//確率で行動変化。
+				auto rand = GRandom().Rand();
+				if (rand < 0.5f) {
+					//タックル。
+					return e->GetState(TO_INT(Boss_Fatman::EnStateEX::enAttackD));
+				}
+				else {
+					//距離をとる。
+					return e->GetState(TO_INT(Boss_Fatman::EnStateEX::enTakeDistance));
+				}
+			}
+		}
 	}
 	return this;
 }
