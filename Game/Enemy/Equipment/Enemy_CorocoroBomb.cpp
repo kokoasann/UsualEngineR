f44bdac637893eb9e3dc65f376d154cfcf6c3de9
kokoasann <kokoasann@gmail.com>
2021-01-18 11:36:23 +0900

    add enemy bomb

---
+++ Game/Enemy/Equipment/Enemy_CorocoroBomb.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "Enemy_CorocoroBomb.h"
+
+
+Enemy_CorocoroBomb::Enemy_CorocoroBomb()
+{
+
+}
+
+Enemy_CorocoroBomb::~Enemy_CorocoroBomb()
+{
+
+}
+
+
+
+void Enemy_CorocoroBomb::Release()
+{
+
+}
+
+void Enemy_CorocoroBomb::OnDestroy()
+{
+	Release();
+}
+
+
+void Enemy_CorocoroBomb::Awake()
+{
+
+}
+
+bool Enemy_CorocoroBomb::Start()
+{
+	return true;
+}
+
+
+void Enemy_CorocoroBomb::PreUpdate()
+{
+
+}
+
+void Enemy_CorocoroBomb::Update()
+{
+
+}
+
+void Enemy_CorocoroBomb::PostUpdate()
+{
+
+}
+
+
+
---
+++ Game/Enemy/Equipment/Enemy_LaunchBomb.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "Enemy_LaunchBomb.h"
+
+
+Enemy_LaunchBomb::Enemy_LaunchBomb()
+{
+
+}
+
+Enemy_LaunchBomb::~Enemy_LaunchBomb()
+{
+
+}
+
+
+
+void Enemy_LaunchBomb::Release()
+{
+
+}
+
+void Enemy_LaunchBomb::OnDestroy()
+{
+	Release();
+}
+
+
+void Enemy_LaunchBomb::Awake()
+{
+
+}
+
+bool Enemy_LaunchBomb::Start()
+{
+	return true;
+}
+
+
+void Enemy_LaunchBomb::PreUpdate()
+{
+
+}
+
+void Enemy_LaunchBomb::Update()
+{
+
+}
+
+void Enemy_LaunchBomb::PostUpdate()
+{
+
+}
+
+
+
