da3e4a24c9a00c8f865fc95698d0914b5edd280d
kokoasann <kokoasann@gmail.com>
2021-01-18 15:05:03 +0900

    boss bomb

--- Game/Enemy/State/MiddleBombState/BossBombIdleState.cpp
+++ Game/Enemy/State/MiddleBombState/BossBombIdleState.cpp
@@ -2,6 +2,8 @@
 #include "BossBombIdleState.h"
 #include "Enemy/IEnemy.h"
 #include "Enemy/EnemyManager.h"
+#include "Enemy/Boss/Boss_MiddleBomb.h"
+#include "GameManager.h"
 
 BossBombIdleState::BossBombIdleState()
 {
@@ -13,10 +15,25 @@ BossBombIdleState::~BossBombIdleState()
 
 void BossBombIdleState::Enter(IEnemy* e)
 {
+	e->PlayAnimation(TO_INT(IEnemy::EnAnimation::enIdle));
+
+	e->SetVelocity(Vector3::Zero);
 }
 
 IEnemyState* BossBombIdleState::Update(IEnemy* e)
 {
+	if(m_isAttacked)
+		return e->GetState(TO_INT(IEnemy::EnState::enBattleState));
+
+	e->SetExternalVelocity({ 0,-500,0 });
+	
+	auto p = GameManager::GetInstance().GetPlayer();
+	auto d = e->GetPosition() - p->GetPosition();
+	float len = d.Length();
+	if (len < 500.)
+	{
+		return e->GetState(TO_INT(IEnemy::EnState::enBattleState));
+	}
 	return this;
 }
 
@@ -26,4 +43,5 @@ void BossBombIdleState::Exit(IEnemy* e)
 
 void BossBombIdleState::OnAttacked(IEnemy* e)
 {
+	m_isAttacked = true;
 }
