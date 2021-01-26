dddfba03174977dfc8780c5285820587544deef7
kbc18b24 <kbc18b24@stu.kawahara.ac.jp>
2021-01-26 14:03:22 +0900

    Boss_FatmanAngryPerform追加

---
+++ Game/Enemy/State/BossFatmanState/Boss_FatmanAngryPerform.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "Boss_FatmanAngryPerform.h"
+
+
+Boss_FatmanAngryPerform::Boss_FatmanAngryPerform()
+{
+
+}
+
+Boss_FatmanAngryPerform::~Boss_FatmanAngryPerform()
+{
+
+}
+
+
+
+void Boss_FatmanAngryPerform::Release()
+{
+
+}
+
+void Boss_FatmanAngryPerform::OnDestroy()
+{
+	Release();
+}
+
+
+void Boss_FatmanAngryPerform::Awake()
+{
+
+}
+
+bool Boss_FatmanAngryPerform::Start()
+{
+	return true;
+}
+
+
+
+void Boss_FatmanAngryPerform::Update()
+{
+
+}
+
+
+
+
