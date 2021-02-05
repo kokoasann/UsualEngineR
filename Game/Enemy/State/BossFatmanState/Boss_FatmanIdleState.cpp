5afb098b7ef98c46a3bb118edaabfd01f7c58fcb
kbc18b24 <kbc18b24@stu.kawahara.ac.jp>
2021-02-05 12:43:25 +0900

    見つける距離の修正

--- Game/Enemy/State/BossFatmanState/Boss_FatmanIdleState.cpp
+++ Game/Enemy/State/BossFatmanState/Boss_FatmanIdleState.cpp
@@ -13,10 +13,30 @@ Boss_FatmanIdleState::~Boss_FatmanIdleState()
 
 void Boss_FatmanIdleState::Enter(IEnemy* e)
 {
+	e->PlayAnimation(IEnemy::EnAnimation::enIdle);
 }
 
 IEnemyState* Boss_FatmanIdleState::Update(IEnemy* e)
 {
+	static float time = 0.f;
+	time += gameTime()->GetDeltaTime();
+	auto add = std::sin(time);
+	Vector3 vel = Vector3::Zero;
+	vel.x = add;
+	vel.y -= 10.f;
+	//e->SetVelocity(vel);
+
+	e->SetVelocity({ 0,-30,0 });
+
+	auto player = GameManager::GetInstance().m_player;
+	auto& epos = e->GetPosition();
+	auto& ppos = player->GetPosition();
+	auto vecToPlayer = ppos - epos;
+	const float chaseRange = 100.f;
+
+	if (vecToPlayer.Length() < chaseRange and player->GetCurrentHP() > 0.f) {
+		return e->GetState(TO_INT(IEnemy::EnState::enBattleState));
+	}
 	return this;
 }
 
