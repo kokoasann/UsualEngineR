aab735364991192e95eda6044d9353d18d3ea985
kokoasann <kokoasann@gmail.com>
2021-02-09 14:17:27 +0900

    add ending scene

---
+++ Game/EndingScene.cpp
@@ -0,0 +1,105 @@
+#include "stdafx.h"
+#include "EndingScene.h"
+
+#include "EventMovie/EventMovie.h"
+
+#include "GameManager.h"
+#include "Player/Player.h"
+#include "Goal.h"
+#include "Fade.h"
+#include "Camera/GameCamera.h"
+
+EndingScene::EndingScene()
+{
+
+}
+
+EndingScene::~EndingScene()
+{
+
+}
+
+
+
+void EndingScene::Release()
+{
+
+}
+
+void EndingScene::OnDestroy()
+{
+	Release();
+}
+
+
+void EndingScene::Awake()
+{
+
+}
+
+bool EndingScene::Start()
+{
+	GameManager::GetInstance().GetGameCamera()->SetActive(false);
+	GameManager::GetInstance().GetPlayer()->SetState(GameManager::GetInstance().GetPlayer()->GetState(Player::EnState::))
+	m_movie = NewGO<EventMovie>(0);
+	m_movie->Init("Assets/eventMovie/ed.evm", g_camera3D,
+		[&](const std::string& name)->ModelRender*
+		{
+			ModelRender* res = nullptr;
+			if (std::strcmp(name.c_str(), "exit")==0)
+			{
+				m_exit = GameManager::GetInstance().m_goal->GetModelRender();
+				res = m_exit;
+			}
+			else if (std::strcmp(name.c_str(), "m") == 0)
+			{
+				//GameManager::GetInstance().GetPlayer()->SetActive(false);
+				m_player = &GameManager::GetInstance().GetPlayer()->GetModelRender();
+				res = m_player;
+			}
+			else if (std::strcmp(name.c_str(), "Plane") == 0)
+			{
+				m_plane = NewGO<ModelRender>(0);
+				ModelInitData mid;
+				mid.m_tkmFilePath = "Assets/modelData/plane/plane.tkm";
+				
+				res = m_plane;
+			}
+			return res;
+		},
+		[&](const std::string& name)
+		{
+
+		});
+	
+	Fade::GetInstance().FadeIn();
+
+	return true;
+}
+
+
+void EndingScene::PreUpdate()
+{
+
+}
+
+void EndingScene::Update()
+{
+
+}
+
+void EndingScene::PostUpdate()
+{
+
+}
+
+
+void EndingScene::Render()
+{
+
+}
+
+void EndingScene::PostRender()
+{
+
+}
---
+++ Game/EndingScene.h
@@ -0,0 +1,64 @@
+#pragma once
+
+class EventMovie;
+/// <summary>
+/// 
+/// </summary>
+class EndingScene :public GameObject
+{
+public:
+	EndingScene();
+	virtual ~EndingScene();
+
+	/// <summary>
+	/// 本開放。確保したものを開放するための関数。
+	/// </summary>
+	virtual void Release() override;
+
+	/// <summary>
+	/// このオブジェクトの所持するゲームオブジェクト等を消すための関数。
+	/// 面倒くさければRelease関数と同じでもよい。
+	/// </summary>
+	virtual void OnDestroy() override;
+
+	/// <summary>
+	/// NewGO時に即座に呼ばれる関数。
+	/// コンストラクタの代わりに使おう。
+	/// </summary>
+	void Awake() override;
+
+	/// <summary>
+	/// スタート関数。
+	/// </summary>
+	/// <returns>trueを返さない限り、ずっとStartを呼び続ける</returns>
+	bool Start() override;
+
+	/// <summary>
+	/// 更新。の前に呼ばれる更新。
+	/// </summary>
+	void PreUpdate() override;
+	/// <summary>
+	/// 更新。
+	/// </summary>
+	void Update() override;
+	/// <summary>
+	/// 更新。の後に呼ばれる更新。
+	/// </summary>
+	void PostUpdate() override;
+
+	/// <summary>
+	/// 描画。
+	/// </summary>
+	void Render() override;
+
+	/// <summary>
+	/// 手前に表示するやつの描画。
+	/// </summary>
+	void PostRender() override;
+private:
+	EventMovie* m_movie;
+
+	ModelRender* m_player;
+	ModelRender* m_exit;
+	ModelRender* m_plane;
+};
